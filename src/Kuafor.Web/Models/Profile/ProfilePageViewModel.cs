e28e602ca9f0c4475ddb0a6744ad97303070c0b7
ibrahim taskin <ibrahimtaskin3807@gmail.com>
2025-08-19 09:21:15 +0300

    Add Loyalty and Coupons to ProfilePageViewModel
    Introduces LoyaltyViewModel and CouponsViewModel properties to ProfilePageViewModel, enabling support for loyalty and coupon features on the profile page.

--- src/Kuafor.Web/Models/Profile/ProfilePageViewModel.cs
+++ src/Kuafor.Web/Models/Profile/ProfilePageViewModel.cs
@@ -5,9 +5,11 @@ namespace Kuafor.Web.Models.Profile
 {
     public class ProfilePageViewModel
     {
-        public IdentityViewModel      Identity      { get; set; } = new();
-        public AddressesViewModel     Addresses     { get; set; } = new();
+        public IdentityViewModel Identity { get; set; } = new();
+        public AddressesViewModel Addresses { get; set; } = new();
         public NotificationsViewModel Notifications { get; set; } = new();
-        public PreferencesViewModel   Preferences   { get; set; } = new();
+        public PreferencesViewModel Preferences { get; set; } = new();
+        public LoyaltyViewModel Loyalty { get; set; } = new();
+        public CouponsViewModel Coupons { get; set; } = new();
     }
 }
