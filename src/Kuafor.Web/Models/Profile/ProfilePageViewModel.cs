3f23e2a7a6abcfeacae4de4299b3b99902b6fb20
ibrahim taskin <ibrahimtaskin3807@gmail.com>
2025-08-19 08:03:00 +0300

    Add PreferencesViewModel to ProfilePageViewModel
    Introduced a new PreferencesViewModel property to ProfilePageViewModel to support user preferences on the profile page. Also improved code formatting for property alignment.

--- src/Kuafor.Web/Models/Profile/ProfilePageViewModel.cs
+++ src/Kuafor.Web/Models/Profile/ProfilePageViewModel.cs
@@ -5,8 +5,9 @@ namespace Kuafor.Web.Models.Profile
 {
     public class ProfilePageViewModel
     {
-        public IdentityViewModel Identity { get; set; } = new();
-        public AddressesViewModel Addresses { get; set; } = new();
+        public IdentityViewModel      Identity      { get; set; } = new();
+        public AddressesViewModel     Addresses     { get; set; } = new();
         public NotificationsViewModel Notifications { get; set; } = new();
+        public PreferencesViewModel   Preferences   { get; set; } = new();
     }
 }
